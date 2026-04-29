ab4d2c82177e9893c91178bfacca5295920d7161
Kitamotoka <il.glazkova08@gmail.com>
2026-04-29 19:02:10 +0300

    add infrastructure

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-let state = { res: 0, level: 1 };
+let state = {
+  res: 0,
+  level: 1,
+  buildings: [{ name: "Vapor Collector", base: 15, rate: 1.15, count: 0 }],
+};
 
 window.onload = () => {
-    document.getElementById('planet').onclick = () => {
-        state.res += 1;
-        render();
-    };
+  document.getElementById("planet").onclick = () => {
+    state.res += 1;
+    render();
+  };
 };
 
 function render() {
-    document.getElementById('resource-display').innerText = `${state.res} 💧`;
+  document.getElementById("resource-display").innerText = `${state.res} 💧`;
 }
