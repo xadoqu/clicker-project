5a555d197065aa564a6748765382f748d115957d
Kitamotoka <il.glazkova08@gmail.com>
2026-05-07 00:48:39 +0300

    lab9 conditional logging based on lvls

--- logger.js
+++ logger.js
@@ -1,12 +1,17 @@
-const LogLevel = { INFO: 'INFO', DEBUG: 'DEBUG', ERROR: 'ERROR' };
+const LogConfig = { currentLevel: LogLevel.INFO };
 
 function withLogging(level, fn) {
     return async function(...args) {
+        const levels = [LogLevel.DEBUG, LogLevel.INFO, LogLevel.ERROR];
         const timestamp = new Date().toISOString();
         const start = performance.now();
         const result = await fn.apply(this, args);
         const duration = (performance.now() - start).toFixed(2);
+        if(levels.indexOf(level) >= levels.indexOf(LogConfig.currentLevel)) {
+            return await fn.apply(this, args);
+        }
         console.log(`[${timestamp}] [${level}] Initializing call... Duration: ${duration}ms`);
+        const start = performance.now();
         return result;
     };
 }
