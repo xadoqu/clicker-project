a734b602d4ce175ed39cf7022750667243f13729
Xadoqu <xadoqu@gmail.com>
2026-05-07 00:11:39 +0300

    lab9 log function

--- logger.js
+++ logger.js
@@ -2,7 +2,9 @@ const LogLevel = { INFO: 'INFO', DEBUG: 'DEBUG', ERROR: 'ERROR' };
 
 function withLogging(level, fn) {
     return function(...args) {
-        console.log(`[${level}] Calling function...`);
-        return fn.apply(this, args);
+        console.log(`[${level}] Args:`, args);
+        const result = fn.apply(this, args);
+        console.log(`[${level}] Result:`, result);
+        return result;
     };
 }
