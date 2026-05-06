38fa0fd07eeeff9e5f2a7ad8fcbf439e08bdd5e5
Xadoqu <xadoqu@gmail.com>
2026-05-07 00:20:20 +0300

    lab9 sync async function

--- logger.js
+++ logger.js
@@ -1,10 +1,15 @@
 const LogLevel = { INFO: 'INFO', DEBUG: 'DEBUG', ERROR: 'ERROR' };
 
 function withLogging(level, fn) {
-    return function(...args) {
-        console.log(`[${level}] Args:`, args);
-        const result = fn.apply(this, args);
-        console.log(`[${level}] Result:`, result);
-        return result;
+    return async function(...args) {
+        console.log(`[${level}] Initializing call...`);
+        try {
+            const result = await fn.apply(this, args);
+            console.log(`[${level}] Result:`, result);
+            return result;
+        } catch (error) {
+            console.error(`[${level} Critical Error] ${error.message}`);
+            throw error;
+        }
     };
 }
