b10fc3b17ab28826b85c5ff7ba5a4b54f6884f0c
Xadoqu <xadoqu@gmail.com>
2026-05-06 22:30:40 +0300

    lab8 automated credential injection

--- api.js
+++ api.js
@@ -1,12 +1,22 @@
 class ApiProxy {
-  constructor(baseURL) {
+  constructor(baseURL, apiKey) {
     this.baseURL = baseURL;
+    this.apiKey = apiKey;
   }
 
   async request(endpoint, options = {}) {
     const url = `${this.baseURL}${endpoint}`;
-    console.log(`[Proxy] Intercepting request to: ${url}`);
-    return fetch(url, options);
+    const authOptions = {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${this.apiKey}`,
+        "Content-Type": "application/json",
+        "X-Game-Version": "1.0.0",
+      },
+    };
+    console.log(`[Proxy] Intercepting request to: ${endpoint}`);
+    return fetch(url, authOptions);
   }
 }
 const apiService = new ApiProxy("https://api.yourgame.com/v1");
