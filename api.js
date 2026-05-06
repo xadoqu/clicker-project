226fbeb7519a79e25f1cb8c1cbb26b26a6574cd0
Kitamotoka <il.glazkova08@gmail.com>
2026-05-06 22:42:37 +0300

    lab8 support multiple auth strategies

--- api.js
+++ api.js
@@ -1,22 +1,26 @@
 class ApiProxy {
-  constructor(baseURL, apiKey) {
+  constructor(baseURL) {
     this.baseURL = baseURL;
-    this.apiKey = apiKey;
+    this.strategy = "API_KEY";
+    this.token = "SECRET_123";
+  }
+
+  setStrategy(newStrategy, newToken) {
+    this.strategy = newStrategy;
+    this.token = newToken;
+    console.log(`[Proxy] Authentication strategy updated: ${newStrategy}`);
   }
 
   async request(endpoint, options = {}) {
     const url = `${this.baseURL}${endpoint}`;
-    const authOptions = {
-      ...options,
-      headers: {
-        ...options.headers,
-        Authorization: `Bearer ${this.apiKey}`,
-        "Content-Type": "application/json",
-        "X-Game-Version": "1.0.0",
-      },
-    };
-    console.log(`[Proxy] Intercepting request to: ${endpoint}`);
-    return fetch(url, authOptions);
+    const headers = { ...options.headers };
+
+    if (this.strategy === "JWT") {
+      headers['Authorization'] = `JWT ${this.token}`;
+    } else {
+      headers['X-API-Key'] = this.token;
+    }
+    return fetch(url, { ...options, headers })
   }
 }
-const apiService = new ApiProxy("https://api.yourgame.com/v1");
+const apiService = new ApiProxy("https://api.planet-clicker.com/v1");
